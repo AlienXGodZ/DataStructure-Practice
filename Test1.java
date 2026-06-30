294bd1f57739c4d741161550f22ff23006c6f313
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-30 14:20:41 +0530

    Test1 added

---
+++ Test1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class Test1 {
+    public static void main(String[] args){
+
+        ArrayList<Integer> store = new ArrayList<>();
+
+        store.add(1);
+        store.add(2);
+        store.add(3);
+        store.add(4);
+        store.add(5);
+
+        System.out.println(store);
+
+    }
+}
