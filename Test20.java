e2ed917972a04af26eda7eece6fbd3d27e7586e8
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-30 17:26:53 +0530

    Test20 added

---
+++ Test20.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Test20 {
+  public static void main (String[] args){
+
+      HashSet<String> store = new HashSet<>();
+
+      store.add("BMW");
+      store.add("FORD");
+      store.add("JACKWOR");
+      store.add("AUDIO");
+      store.add("FORD");
+
+      System.out.println(store);
+  }
+}
