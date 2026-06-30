91675f68dcf01118b2f139493ee73c08b8c8f76b
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-30 21:38:39 +0530

    Test43 added

---
+++ Test43.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+
+public class Test43 {
+    public static void main(String[] args){
+
+                TreeMap<Integer,String> store = new TreeMap<>();
+
+                store.put(2,"Allen");
+                store.put(1,"Joan");
+                store.put(3,"Edwin");
+                store.put(4,"Harvey");
+
+
+                for(String i : store.values()){
+                    System.out.println(i);
+                }
+
+
+
+            }
+
+        }
+
+
+
+
