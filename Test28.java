c3792d78a906085d97f9f43f906264becf82eca2
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-30 18:00:35 +0530

    Test28 added

---
+++ Test28.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Test28 {
+    public static void main(String[] args){
+
+        TreeSet<Integer> store = new TreeSet<>();
+
+        store.add(10);
+        store.add(40);
+        store.add(20);
+        store.add(30);
+        store.add(20);
+        store.add(50);
+
+       store.remove(20);
+       System.out.println(store);
+
+
+    }
+
+}
+
