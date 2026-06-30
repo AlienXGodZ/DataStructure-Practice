21892502f6404df4468aba2628d8d663218bc081
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-30 17:48:26 +0530

    Test24 added

---
+++ Test24.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Test24 {
+    public static void main(String[] args){
+
+        TreeSet<Integer> store = new TreeSet<>();
+
+        store.add(10);
+        store.add(40);
+        store.add(30);
+        store.add(20);
+        store.add(50);
+
+        System.out.println(store);
+
+
+    }
+
+}
+
