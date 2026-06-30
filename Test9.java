f8c83a7476827b6a39352975e58617dade3c4f91
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-30 14:58:16 +0530

    Test9 added

---
+++ Test9.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+public class Test9 {
+    public static void main(String[] args){
+
+        ArrayList<Integer> store = new ArrayList<>();
+
+        store.add(10);
+        store.add(20);
+        store.add(30);
+        store.add(40);
+        store.add(50);
+
+        System.out.println(store.contains(30));
+
+
+    }
+
+}
+
