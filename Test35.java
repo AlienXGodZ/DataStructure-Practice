2b6310db651043566682995ec8de58ab43970395
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-30 19:40:06 +0530

    Test35 added

---
+++ Test35.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Test35 {
+    public static void main(String[] args){
+
+        HashMap<Integer,String> store = new HashMap<>();
+
+        store.put(1,"Allen");
+        store.put(2,"Joan");
+        store.put(3,"Harrish");
+
+        store.remove(1);
+
+
+        System.out.println(store);
+
+
+    }
+
+}
+
+
