dec4341089f74df2d0b29ea20cdfcf6154ad80ee
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-30 22:56:43 +0530

    Test46 added

---
+++ Test46.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Test46 {
+    public static void main(String[] args){
+
+        LinkedHashMap<Integer,String> store = new LinkedHashMap<>();
+
+        store.put(1,"Allen");
+        store.put(2,"Joan");
+        store.put(3,"Harrish");
+        store.put(4,"Harvey");
+
+        System.out.println(store);
+
+    }
+}
