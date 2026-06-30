04dd0b8846f9d1fc2cac0bb0c5ecd6e4c613543f
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-30 15:09:34 +0530

    Test13 added

---
+++ Test13.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Test13 {
+    public static void main(String[] args){
+
+        ArrayList<String> store = new ArrayList<String>();
+
+        store.add("Allen");
+        store.add("Joan");
+        store.add("Harrish");
+        store.add("Harvey");
+
+
+        System.out.println(store);
+
+    }
+}
