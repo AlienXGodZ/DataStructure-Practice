fd123d718d94908730e17dea0e3f01aa88605737
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-30 15:26:02 +0530

    Test15 added

---
+++ Test15.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Test15 {
+    public static void main(String[] args){
+
+        LinkedList<String> store = new LinkedList<>();
+
+        store.add("Allen");
+        store.add("Harvey");
+        store.add("Joan");
+        store.add("Harrish");
+        store.add("Stella");
+
+        store.addFirst("Allen1");
+
+        System.out.println(store);
+    }
+}
