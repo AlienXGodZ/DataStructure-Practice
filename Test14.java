d68d67a221becaaf7f765230d836d84daa856b5d
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-30 15:18:49 +0530

    Test14 added

---
+++ Test14.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Test14 {
+    public static void main(String[] args){
+
+        List<String> store = new LinkedList<>();
+
+        store.add("Allen");
+        store.add("Harvey");
+        store.add("Joan");
+        store.add("Harrish");
+        store.add("Stella");
+
+        System.out.println(store);
+    }
+}
