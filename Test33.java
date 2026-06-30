6b8a5dff0a00e89ce9bd0bf57768a7321f914e3f
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-30 19:32:03 +0530

    Test33 added

---
+++ Test33.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Test33 {
+    public static void main(String[] args){
+
+        HashMap<Integer,String> store = new HashMap<>();
+
+        store.put(1,"Allen");
+        store.put(2,"Joan");
+        store.put(3,"Harrish");
+
+
+            System.out.println(store.get(1));
+
+
+        }
+
+    }
+
+
