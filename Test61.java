ca97cd7cbe0b0078340835778e14c5739b4c8ca8
AlienXGodZ <allenharvey2005@gmail.com>
2026-07-01 23:47:58 +0530

    Test61 added

---
+++ Test61.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Test61 {
+    public static void main(String[] args){
+
+        ArrayList<String> store = new ArrayList<>();
+
+        store.add("Joan");
+        store.add("Allen");
+        store.add("Vinoth");
+        store.add("Harvey");
+        store.add("Bala");
+
+        Collections.sort(store);
+
+        System.out.println(store);
+
+
+
+
+    }
+}
