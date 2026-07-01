bdb376b06412b5f641ac26911ba2715871f62488
AlienXGodZ <allenharvey2005@gmail.com>
2026-07-01 23:18:14 +0530

    Test56 added

--- Test56.java
+++ Test56.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class Test56 {
+    public static void main(String[] args){
+
+        HashSet<String> removeDuplicate = new HashSet<>();
+
+        removeDuplicate.add("Allen");
+        removeDuplicate.add("Joan");
+        removeDuplicate.add("Harvey");
+        removeDuplicate.add("Harrish");
+
+        System.out.println("HashSet: " + removeDuplicate);
+
+        ArrayList<String> fromHashSet = new ArrayList<>(removeDuplicate);
+
+        System.out.println("ArrayList: " + fromHashSet);
+    }
 }
