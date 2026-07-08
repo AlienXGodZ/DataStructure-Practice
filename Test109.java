31c94291fb74d3a0a9854d38e56e27bdccdd4858
AlienXGodZ <allenharvey2005@gmail.com>
2026-07-08 14:24:00 +0530

    Test109 added

---
+++ Test109.java
@@ -0,0 +1,100 @@
+import java.util.*;
+
+ class queuepatient{
+
+     String name;
+     int age;
+
+     queuepatient(String name,int age){
+
+         this.name = name;
+         this.age = age;
+     }
+
+     public String toString(){
+         return "PATIENT NAME: " + name + " | PATIENT AGE: " + age;
+     }
+ }
+
+   class queuemanagementsystem{
+
+      Queue<queuepatient> store = new LinkedList<>();
+
+      void add(queuepatient s){
+          store.offer(s);
+      }
+
+     void servePatient(){
+
+          if(store.isEmpty()){
+              System.out.println("\nNo Patient in Queue");
+          }else{
+              System.out.println("\nServed Patient:" + store.poll());
+          }
+
+     }
+
+     void nextPatient(){
+
+         if(store.isEmpty()){
+             System.out.println("\nNo Patient in Queue");
+         }else{
+             System.out.println("\nNext Patient:" + store.peek());
+         }
+     }
+
+     void display() {
+
+         System.out.println("\nNEXT PATIENTLIST: ");
+
+         if (store.isEmpty()) {
+             System.out.println("Queue is Empty");
+             return;
+         } else {
+
+             for (queuepatient i : store) {
+                 System.out.println(i);
+             }
+         }
+     }
+
+     void countPatient(){
+
+          System.out.println("\nNumber of TotalPatient: " + store.size());
+
+     }
+
+     void checkEmpty(){
+
+          if(store.isEmpty()){
+              System.out.println("\nQueue Empty");
+          }else{
+              System.out.println("\nQueue is not Empty");
+          }
+     }
+   }
+
+public class Test109 {
+    public static void main(String[] args){
+
+        queuemanagementsystem s = new queuemanagementsystem();
+
+        s.add(new queuepatient("ALLEN",15));
+        s.add(new queuepatient("JOAN",18));
+        s.add(new queuepatient("HARVEY",20));
+        s.add(new queuepatient("HARRISH",25));
+        s.add(new queuepatient("BALA",30));
+
+
+        s.servePatient();
+
+        s.nextPatient();
+
+        s.countPatient();
+
+        s.checkEmpty();
+
+        s.display();
+
+    }
+}
